5af728fac6c00a3b1d3e67f6575676357f98ac1d
Kirill Berezin <enelar@develop-project.ru>
2015-07-02 21:27:51 +0300

    Check status on the same connection

--- sql/loader.php
+++ sql/loader.php
@@ -4,16 +4,17 @@ function SQLLoad($file)
 {
   header("SQL: $file", false);
 
+  $con = db::RawConnection();
   $sql = file_get_contents($file);
-  pg_send_query(db::RawConnection(), $sql);
-  if (($error = pg_last_error()) !== '')
+  pg_send_query($con, $sql);
+  if (($error = pg_last_error($con)) !== '')
     die("Failure at loading {{$file}} with {{$error}}");
 
-  while (pg_connection_busy())
+  while (pg_connection_busy($con))
     sleep(1);
 
-  while (pg_get_result() !== false)
-    if (($error = pg_last_error()) !== '')
+  while (pg_get_result($con) !== false)
+    if (($error = pg_last_error($con)) !== '')
       die("Failure at loading {{$file}} with {{$error}}");
 }
 
