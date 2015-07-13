a0dbb19a05ea7e85e9b4c4871495ff73c0688ad5
Kirill Berezin <enelar@exsul.net>
2015-07-13 18:09:55 +0300

    AutoInstance database on fresh install

--- sql/loader.php
+++ sql/loader.php
@@ -1,10 +1,42 @@
 <?php
 
+function EmergencyCreateDB()
+{
+  GLOBAL $sql;
+
+  $str = $sql->con_params['origin'];
+
+  $start = 0;
+  for (; $start < strlen($str); $start++)
+    if ($str[$start] == '@')
+      break;
+  for (; $start < strlen($str); $start++)
+    if ($str[$start] == '/')
+      break;
+  for ($end = $start; $end < strlen($str); $end++)
+    if ($end == ':')
+      break;
+
+  $emergency_connection_string =
+    substr($str, 0, $start + 1)
+    .
+    substr($str, $end);
+
+  var_dump("Emergency connect: {$emergency_connection_string}");
+  $pg = $sql->Connect($emergency_connection_string);
+
+  $result = $pg->Query("CREATE DATABASE obmen");
+
+  var_dump("Reload page please");
+  exit();
+}
+
 function SQLLoad($file)
 {
   header("SQL: $file", false);
 
   $con = db::RawConnection();
+
   $sql = file_get_contents($file);
   pg_send_query($con, $sql);
   if (($error = pg_last_error($con)) !== '')
@@ -18,10 +50,13 @@ function SQLLoad($file)
       die("Failure at loading {{$file}} with {{$error}}");
 }
 
+if (db::RawConnection() == false)
+  EmergencyCreateDB();
+
 $res = @db::Query("SELECT * FROM users.info WHERE uid=0");
-if (pg_last_error() != "")
+if (is_string($res)) // then its error
   SQLLoad("sql/schema.sql");
-if (!$res())
+else if (!$res())
 {
   SQLLoad("sql/initial.sql");
 
