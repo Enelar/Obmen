4dd3ea2973875289bab898c4bcbf3f6e6774b805
Kirill Berezin <enelar@exsul.net>
2015-07-20 07:28:59 +0300

    Fix enjs requirment missplaced

--- engine/warmup.js
+++ engine/warmup.js
@@ -5,6 +5,8 @@ var warmup_obj =
   skip_initiation: true,
   OnWaiting: function()
   {
+    phoxy._EarlyStage.sync_require[0] = "/enjs.js";
+
     phoxy._EarlyStage.sync_require.push("//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js");
     phoxy._EarlyStage.EntryPoint();
     phoxy.state.early.optional.lazy = 4;
