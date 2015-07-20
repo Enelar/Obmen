f0f280a1686ade4056e8a754a563b71513f68132
Kirill Berezin <enelar@exsul.net>
2015-07-20 07:03:55 +0300

    Move from git submodules to composer

--- css/compile.php
+++ css/compile.php
@@ -1,10 +1,10 @@
 <?php
-
 include("../vendor/autoload.php");
 
 $less = new lessc;
 
 header("Content-Type: text/css");
+header('Cache-Control: public, max-age=3600');
 try
 {
   echo $less->compileFile('a.less');
